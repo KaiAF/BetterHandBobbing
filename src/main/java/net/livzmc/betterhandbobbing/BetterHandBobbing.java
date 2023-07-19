435b07f0bf97d0a040e63820ecdc03201191b0d5
iris <60985521+KaiAF@users.noreply.github.com>
2023-07-18 19:13:02 -0500

    fix incompatibility with quilt

--- src/main/java/net/livzmc/betterhandbobbing/BetterHandBobbing.java
+++ src/main/java/net/livzmc/betterhandbobbing/BetterHandBobbing.java
@@ -1,15 +1,19 @@
 package net.livzmc.betterhandbobbing;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.livzmc.betterhandbobbing.util.Config;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BetterHandBobbing implements ModInitializer {
     public static final Config config = new Config();
+    public static final Logger LOGGER = LogManager.getLogger("BetterHandBobbing");
     public static final SimpleOption<Boolean> handBob = SimpleOption.ofBoolean("betterhandbobbing.options.handbob", config.getHandBob(), (bool) -> {
         config.setHandBob(bool);
         config.save();
@@ -17,6 +21,12 @@ public class BetterHandBobbing implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        if (FabricLoader.getInstance().isModLoaded("quilt_loader")) {
+            LOGGER.info("[BetterHandBobbing] Using QuiltMC");
+            if (!FabricLoader.getInstance().isModLoaded("quilted_fabric_api")) {
+                LOGGER.warn("[BetterHandBobbing] Quilted Fabric API was not found! It is suggested to use this so that the language files for my settings will work.");
+            }
+        }
     }
 
     public static SimpleOption<Boolean> getHandBob() {
