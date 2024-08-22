24927d3bd686111d86d885777e451a1c066eceef
iris <60985521+KaiAF@users.noreply.github.com>
2024-08-22 17:34:52 -0500

    mend
    fix merge conflicts

--- src/main/java/net/livzmc/betterhandbobbing/mixin/VideoOptionsScreenMixin.java
+++ src/main/java/net/livzmc/betterhandbobbing/mixin/VideoOptionsScreenMixin.java
@@ -1,22 +1,20 @@
 package net.livzmc.betterhandbobbing.mixin;
 
-import net.minecraft.client.OptionInstance;
-import net.minecraft.client.Options;
-import net.minecraft.client.gui.screens.VideoSettingsScreen;
+import net.livzmc.betterhandbobbing.BetterHandBobbing;
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.SimpleOption;
 import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static net.livzmc.betterhandbobbing.BetterHandBobbing.handBob;
-
-@Mixin(VideoSettingsScreen.class)
+@Mixin(VideoOptionsScreen.class)
 public class VideoOptionsScreenMixin {
-
-    @Inject(method = "options", at = @At("RETURN"), cancellable = true)
-    private static void bhb$getOptions(Options gameOptions, CallbackInfoReturnable<OptionInstance<?>[]> cir) {
-        OptionInstance<?>[] values = cir.getReturnValue();
-        cir.setReturnValue(ArrayUtils.insert(9, values, handBob));
+    @Inject(method = "getOptions", at = @At("RETURN"), cancellable = true)
+    private static void bhb$getOptions(GameOptions gameOptions, CallbackInfoReturnable<SimpleOption<?>[]> cir) {
+        SimpleOption<?>[] values = cir.getReturnValue();
+        cir.setReturnValue(ArrayUtils.insert(9, values, BetterHandBobbing.handBob));
     }
 }
